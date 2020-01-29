84a9728210a67a953251dcefa0214cff003f9376
Nader Yasser <naderdd7@gmail.com>
2020-01-29 15:02:40 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# InfiniteScrollComponent
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import List from "./List";
+export default function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6 justify-content-center my-5">
+          <List />
+        </div>
+      </div>
+    </div>
+  );
+}
